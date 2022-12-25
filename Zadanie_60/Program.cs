2dc96f2339f871f1cbe8e67cfb91247354460da4
Badri <9299547077@mail.ru>
2022-12-25 23:32:59 +0300

    Вывод трехмерного массива с индексом

---
+++ Zadanie_60/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 60. ...Сформируйте трёхмерный массив из двузначных чисел.
+//  Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
+// Массив размером 2 x 2 x 2
+// 66(0,0,0) 25(0,1,0)
+// 34(1,0,0) 41(1,1,0)
+// 27(0,0,1) 90(0,1,1)
+// 26(1,0,1) 55(1,1,1)
+
+Console.Clear();
+int m = 3;
+int n = 2;
+int z = 3;
+int[,,] array = FillArray(m, n, z);
+PrintArray2D(array);
+
+
+
+int[,,] FillArray(int m, int n, int z)
+{
+    Random rand = new Random();
+    int[,,] array = new int[m, n, z];
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int k = 0; k < array.GetLength(2); k++)
+            {
+                array[i, j, k] = rand.Next(10, 100);
+            }
+        }
+    }
+    return array;
+}
+
+void PrintArray2D(int[,,] arr)
+{
+    for (int i = 0; i < arr.GetLength(0); i++)
+    {
+        for (int j = 0; j < arr.GetLength(1); j++)
+        {
+            for (int k = 0; k < arr.GetLength(2); k++)
+            {
+                System.Console.Write($"{arr[i, j, k]}({i},{j},{k}) ");
+            }
+            System.Console.WriteLine();
+        }
+        System.Console.WriteLine();
+    }
+    System.Console.WriteLine();
+}
